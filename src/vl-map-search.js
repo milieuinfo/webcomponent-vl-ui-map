2101e889f260ddaecaef20c9787e18d8d83fd4bd
Tom Coemans <tom.coemans@gmail.com>
2019-05-29 10:19:01 +0200

    ZEND-1789 dubbele registratie events vermijden

--- src/vl-map-search.js
+++ src/vl-map-search.js
@@ -34,38 +34,44 @@ export class VlMapSearch extends VlElement(HTMLElement) {
     }
 
     _addSearchEventListener() {
-        this._selectElement.addEventListener('search', (event) => {
-            if (event && event.detail && event.detail.value) {
-                fetch('http://loc.geopunt.be/v4/Suggestion?q=' + event.detail.value)
-                    .then((response) => {
-                        return response.json()
-                    }).then((data) => {
-                        if (data && data.SuggestionResult) {
-                            const resultaten = data.SuggestionResult.map((resultaat) => {
-                                return {
-                                    value: resultaat,
-                                    label: resultaat
-                                }
-                            });
-                            this._selectElement.choices = resultaten;
-                        }
-                    });
-            }
-        });
+        if (!this.__searchEventListenerRegistered) {
+            this.__searchEventListenerRegistered = true;
+            this._selectElement.addEventListener('search', (event) => {
+                if (event && event.detail && event.detail.value) {
+                    fetch('http://loc.geopunt.be/v4/Suggestion?q=' + event.detail.value)
+                        .then((response) => {
+                            return response.json();
+                        }).then((data) => {
+                            if (data && data.SuggestionResult) {
+                                const resultaten = data.SuggestionResult.map((resultaat) => {
+                                    return {
+                                        value: resultaat,
+                                        label: resultaat
+                                    }
+                                });
+                                this._selectElement.choices = resultaten;
+                            }
+                        });
+                }
+            });
+        }
     }
 
     _addChoiceEventListener() {
-        this._selectElement.addEventListener('choice', (event) => {
-            if (event && event.detail && event.detail.choice) {
-                fetch('http://loc.geopunt.be/v4/Location?q=' + event.detail.choice.value)
-                    .then((response) => {
-                        return response.json()
-                    }).then((data) => {
-                        if (data && data.LocationResult) {
-                            this._parentElement.zoom([data.LocationResult[0].BoundingBox.LowerLeft.X_Lambert72, data.LocationResult[0].BoundingBox.LowerLeft.Y_Lambert72, data.LocationResult[0].BoundingBox.UpperRight.X_Lambert72, data.LocationResult[0].BoundingBox.UpperRight.Y_Lambert72]);
-                        }
-                    });
-            }
-        });
+        if (!this.__choiceEventListenerRegistered) {
+            this.__choiceEventListenerRegistered = true;
+            this.__choiceEventListener = this._selectElement.addEventListener('choice', (event) => {
+                if (event && event.detail && event.detail.choice) {
+                    fetch('http://loc.geopunt.be/v4/Location?q=' + event.detail.choice.value)
+                        .then((response) => {
+                            return response.json();
+                        }).then((data) => {
+                            if (data && data.LocationResult) {
+                                this._parentElement.zoomTo([data.LocationResult[0].BoundingBox.LowerLeft.X_Lambert72, data.LocationResult[0].BoundingBox.LowerLeft.Y_Lambert72, data.LocationResult[0].BoundingBox.UpperRight.X_Lambert72, data.LocationResult[0].BoundingBox.UpperRight.Y_Lambert72]);
+                            }
+                        });
+                }
+            });
+        }
     }
 }
