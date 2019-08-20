1b83e487335d00664e981266b4ad13e235827a17
cambiph <incubusattax@gmail.com>
2019-08-20 10:29:22 +0200

    Voeg link naar release notes toe aan documentatie (#31)
    * Voeg link naar release notes toe aan documentatie

--- src/vl-map-baselayer-grb-ortho.js
+++ src/vl-map-baselayer-grb-ortho.js
@@ -6,6 +6,8 @@ import { VlMapBaseLayer } from "./vl-map-baselayer.js";
  * @classdesc De kaart basis laag component voor GRB ortho. <a href="demo/vl-map.html">Demo</a>.
  * 
  * @extends VlElement
+ * 
+ *  @version <a href="http://www.github.com/milieuinfo/webcomponent-vl-ui-map/releases/latest">Release notes</a>
  */
 export class VlMapBaseLayerGRBOrtho extends VlMapBaseLayer {
     constructor() {
