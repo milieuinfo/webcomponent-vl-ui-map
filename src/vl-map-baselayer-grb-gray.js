400a2177d6eb6097c4551ba58330e91228b077fb
cambiph <incubusattax@gmail.com>
2019-09-05 11:22:02 +0200

    UIG-218

--- src/vl-map-baselayer-grb-gray.js
+++ src/vl-map-baselayer-grb-gray.js
@@ -3,12 +3,13 @@ import { VlMapBaseLayer } from "./vl-map-baselayer.js";
 /**
  * VlMapBaseLayerGRBGray
  * @class
- * @classdesc De kaart basis laag component voor GRB grijstinten. <a href="demo/vl-map.html">Demo</a>.
+ * @classdesc De kaart basis laag component voor GRB grijstinten.
  *
  * @extends VlElement
  * 
  * @see {@link https://www.github.com/milieuinfo/webcomponent-vl-ui-map/releases/latest|Release notes}
  * @see {@link https://www.github.com/milieuinfo/webcomponent-vl-ui-map/issues|Issues}
+ * @see {@link https://webcomponenten.omgeving.vlaanderen.be/demo/vl-ui-map.html|Demo}
  */
 export class VlMapBaseLayerGRBGray extends VlMapBaseLayer {
     constructor() {
