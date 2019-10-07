29b74f67876b174594ded6ba3332f5ddfc51cbee
zhaodifeng <zhaodi.feng@vlaanderen.be>
2019-10-07 10:05:34 +0200

    Merge with #63

--- src/vl-map.js
+++ src/vl-map.js
@@ -1,4 +1,4 @@
-import {VlElement} from "/node_modules/vl-ui-core/vl-core.js";
+import { VlElement } from "/node_modules/vl-ui-core/vl-core.js";
 import style from './vl-map.scss';
 
 /**
@@ -7,7 +7,7 @@ import style from './vl-map.scss';
  * @classdesc De kaart component.
  *
  * @extends VlElement
- *
+ * 
  * @property {boolean} disable-escape-key - Attribuut wordt gebruikt om ervoor te zorgen dat de escape toets niet gebruikt kan worden.
  *
  * @example De map krijgt een default hoogte 500px. Wij kunnen dit hoogte overschrijven door volgende property.
@@ -27,8 +27,8 @@ import style from './vl-map.scss';
  * @see {@link https://webcomponenten.omgeving.vlaanderen.be/demo/vl-map.html|Demo}
  */
 export class VlMap extends VlElement(HTMLElement) {
-  constructor() {
-    super(`
+    constructor() {
+        super(`
             <style>
                 :host {
                     display: none;
@@ -41,71 +41,71 @@ export class VlMap extends VlElement(HTMLElement) {
             <div id="map"></div>
         `);
 
-    this.__updateMapSizeOnLoad();
-    this.__updateOverviewMapSizeOnLoad();
-  }
-
-  /**
-   * Geeft de OpenLayers map terug.
-   *
-   * @Return {acd.ol.CustomMap}
-   */
-  get map() {
-    return this._map;
-  }
-
-  get disableEscapeKey() {
-    return this.getAttribute('disable-escape-key') != undefined;
-  }
-
-  get _geoJSON() {
-    if (!this.__geoJSON) {
-      this.__geoJSON = new ol.format.GeoJSON();
+        this.__updateMapSizeOnLoad();
+        this.__updateOverviewMapSizeOnLoad();
     }
-    return this.__geoJSON;
-  }
+    
+    /**
+     * Geeft de OpenLayers map terug.
+     * 
+     * @Return {acd.ol.CustomMap}
+     */
+    get map() {
+        return this._map;
+    }
 
-  get _mapElement() {
-    return this._shadow.querySelector('#map');
-  }
+    get disableEscapeKey() {
+        return this.getAttribute('disable-escape-key') != undefined;
+    }
 
-  get _projection() {
-    return new ol.proj.Projection({
-      code: 'EPSG:31370',
-      extent: this._extent
-    });
-  }
+    get _geoJSON() {
+        if (!this.__geoJSON) {
+            this.__geoJSON = new ol.format.GeoJSON();
+        }
+        return this.__geoJSON;
+    }
 
-  get _extent() {
-    return [9928, 66928, 272072, 329072];
-  }
+    get _mapElement() {
+        return this._shadow.querySelector('#map');
+    }
 
-  connectedCallback() {
-    this.__initializeCoordinateSystem();
+    get _projection() {
+        return new ol.proj.Projection({
+            code: 'EPSG:31370',
+            extent: this._extent
+        });
+    }
 
-    this._map = new acd.ol.CustomMap({
-      actions: [],
-      disableEscapeKey: this.disableEscapeKey,
-      customLayers: {
-        baseLayerGroup: this.__createLayerGroup('Basis lagen', []),
-        overviewMapLayers: [],
-        overlayGroup: this.__createLayerGroup('Lagen', [])
-      },
-      projection: this._projection,
-      target: this._mapElement
-    });
+    get _extent() {
+        return [9928, 66928, 272072, 329072];
+    }
 
-    this._map.initializeView();
-  }
+    connectedCallback() {
+        this.__initializeCoordinateSystem();
 
-  /**
-   * Voegt een kaartactie toe aan de kaart.
-   *
-   * @param {ol.interaction} action
-   */
-  addAction(action) {
-    this._map.addAction(action);
-  }
+        this._map = new acd.ol.CustomMap({
+            actions: [],
+            disableEscapeKey: this.disableEscapeKey,
+            customLayers: {
+                baseLayerGroup: this.__createLayerGroup('Basis lagen', []),
+                overviewMapLayers: [],
+                overlayGroup: this.__createLayerGroup('Lagen', [])
+            },
+            projection: this._projection,
+            target: this._mapElement
+        });
+
+        this._map.initializeView();
+    }
+
+    /**
+     * Voegt een kaartactie toe aan de kaart.
+     * 
+     * @param {ol.interaction} action 
+     */
+    addAction(action) {
+        this._map.addAction(action);
+    }
 
   /**
    * Disable een kaartactie
@@ -118,8 +118,15 @@ export class VlMap extends VlElement(HTMLElement) {
    *https://openlayers.org/en/latest/apidoc/module-ol_interaction_Interaction-Interaction.html
    *@param {ol.interaction.Pointer} action
    */
-  disableAction(action) {
-    this._setAction(action,false);
+  _disableAction(action) {
+    this._setAction(action, false);
+  }
+
+  /**
+   * Disable pinchRotate
+   */
+  disablePinchRotate() {
+    this._disableAction(ol.interaction.PinchRotate)
   }
 
   /**
@@ -144,38 +151,35 @@ export class VlMap extends VlElement(HTMLElement) {
     this._map.zoomToExtent(boundingbox);
   }
 
-  __updateMapSize() {
-    this.style.display = 'block';
-    if (this._map) {
-      this._map.updateSize();
+    __updateMapSize() {
+        this.style.display = 'block';
+        if (this._map) {
+            this._map.updateSize();
+        }
     }
-  }
 
-  __updateOverviewMapSize() {
-    if (this._map.overviewMapControl) {
-      this._map.overviewMapControl.getOverviewMap().updateSize();
+    __updateOverviewMapSize() {
+        if (this._map.overviewMapControl) {
+            this._map.overviewMapControl.getOverviewMap().updateSize();
+        }
     }
-  }
 
-  __updateOverviewMapSizeOnLoad() {
-    window.addEventListener('load', this.__updateOverviewMapSize.bind(this),
-        {once: true});
-  }
+    __updateOverviewMapSizeOnLoad() {
+        window.addEventListener('load', this.__updateOverviewMapSize.bind(this), { once: true });
+    }
 
-  __updateMapSizeOnLoad() {
-    window.addEventListener('load', this.__updateMapSize.bind(this),
-        {once: true});
-  }
+    __updateMapSizeOnLoad() {
+        window.addEventListener('load', this.__updateMapSize.bind(this), { once: true });
+    }
 
-  __createLayerGroup(title, layers) {
-    return new ol.layer.Group({
-      title: title,
-      layers: layers
-    });
-  }
+    __createLayerGroup(title, layers) {
+        return new ol.layer.Group({
+            title: title,
+            layers: layers
+        });
+    }
 
-  __initializeCoordinateSystem() {
-    proj4.defs('EPSG:31370',
-        '+proj=lcc +lat_1=51.16666723333333 +lat_2=49.8333339 +lat_0=90 +lon_0=4.367486666666666 +x_0=150000.013 +y_0=5400088.438 +ellps=intl +towgs84=-106.869,52.2978,-103.724,0.3366,-0.457,1.8422,-1.2747 +units=m +no_defs');
-  }
+    __initializeCoordinateSystem() {
+        proj4.defs('EPSG:31370', '+proj=lcc +lat_1=51.16666723333333 +lat_2=49.8333339 +lat_0=90 +lon_0=4.367486666666666 +x_0=150000.013 +y_0=5400088.438 +ellps=intl +towgs84=-106.869,52.2978,-103.724,0.3366,-0.457,1.8422,-1.2747 +units=m +no_defs');
+    }
 }
