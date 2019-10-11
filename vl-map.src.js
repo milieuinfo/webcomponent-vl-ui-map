175de5a17a5468a1183852656350c695af59a645
zhaodifeng <zhaodi.feng@vlaanderen.be>
2019-10-11 08:38:02 +0200

    fixed dependency issue

--- vl-map.src.js
+++ vl-map.src.js
@@ -14,7 +14,7 @@ import {VlMapSearch} from "./src/vl-map-search";
 (() => {
     loadScript('VlMap-openlayers', '/node_modules/vl-mapactions/lib/openlayers/dist/ol.js');
     loadScript('VlMap-proj4', '/node_modules/proj4/dist/proj4.js');
-    loadScript('VlMap-mapactions', '/node_modules/vl-mapactions/dist/mapactions.min.js', () => {
+    loadScript('VlMap-mapactions', '/node_modules/vl-mapactions/dist/mapactions-min.js', () => {
         customElements.define('vl-map', VlMap);
         customElements.define('vl-map-overview-map',VlMapOverviewMap);
         customElements.define('vl-map-layer', VlMapLayer);
