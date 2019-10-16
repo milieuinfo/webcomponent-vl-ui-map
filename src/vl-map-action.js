4a9048736fa2a8b19c64c51b1a2f2214a39c8696
Tom Coemans <tom.coemans@gmail.com>
2019-10-16 15:23:32 +0200

    verkeerde demo links opgelost

--- src/vl-map-action.js
+++ src/vl-map-action.js
@@ -11,7 +11,6 @@ import { VlElement } from "/node_modules/vl-ui-core/vl-core.js";
  * 
  * @see {@link https://www.github.com/milieuinfo/webcomponent-vl-ui-map/releases/latest|Release notes}
  * @see {@link https://www.github.com/milieuinfo/webcomponent-vl-ui-map/issues|Issues}
- * @see {@link https://webcomponenten.omgeving.vlaanderen.be/demo/vl-map.html|Demo}
  */
 export class VlMapAction extends VlElement(HTMLElement) {
     connectedCallback() {
