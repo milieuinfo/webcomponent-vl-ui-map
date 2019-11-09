3faf2f13013bdb0ba3ed46512ff402fa2a3125d3
Tom Coemans <tom.coemans@gmail.com>
2019-11-09 11:28:02 +0100

    onnodige check verwijderd

--- vl-map.js
+++ vl-map.js
@@ -1160,7 +1160,7 @@ class VlMapSearch extends VlElement(HTMLElement) {
 
   _configure() {
     customElements.whenDefined('vl-map').then(() => {
-      if (this.parentNode && this.parentNode.map && (this.parentNode.tagName === 'VL-MAP' || this.parentNode.host.tagName === 'VL-MAP')) {
+      if (this.parentNode && this.parentNode.map) {
         this.parentNode._shadow.prepend(this);
         this.parentNode.host.style.setProperty('--vl-map--margin-top', "35px");
         this._map = this._parentElement;
