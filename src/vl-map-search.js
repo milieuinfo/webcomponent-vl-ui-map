bc13ca87a8d5593bd06ac2ee29f76a6ac5886b6a
Tom Coemans <tom.coemans@gmail.com>
2019-11-09 11:31:01 +0100

    merge

--- src/vl-map-search.js
+++ src/vl-map-search.js
@@ -13,101 +13,101 @@ import '/node_modules/vl-ui-select/vl-select.js';
  * @see {@link https://webcomponenten.omgeving.vlaanderen.be/demo/vl-map-search.html|Demo}
  */
 export class VlMapSearch extends VlElement(HTMLElement) {
-    constructor() {
-        super(`
+  constructor() {
+    super(`
             <style>
                 @import '/node_modules/vl-ui-select/style.css';
             </style>
         `);
-        this._configure();
-        customElements.whenDefined('vl-select').then(() => {
-            this._shadow.appendChild(this._getSelectTemplate());
-            this._addSearchEventListener();
-            this._addChoiceEventListener();
-        });
-    }
+    this._configure();
+    customElements.whenDefined('vl-select').then(() => {
+      this._shadow.appendChild(this._getSelectTemplate());
+      this._addSearchEventListener();
+      this._addChoiceEventListener();
+    });
+  }
 
-    get url() {
-        return 'https://loc.geopunt.be/v4';
-    }
+  get url() {
+    return 'https://loc.geopunt.be/v4';
+  }
 
-    get searchUrl() {
-        return this.url + '/Suggestion?q=';
-    }
+  get searchUrl() {
+    return this.url + '/Suggestion?q=';
+  }
 
-    get locationUrl() {
-        return this.url + '/Location?q=';
-    }
+  get locationUrl() {
+    return this.url + '/Location?q=';
+  }
 
-    get _selectElement() {
-        return this._shadow.querySelector('select');
-    }
+  get _selectElement() {
+    return this._shadow.querySelector('select');
+  }
 
-    get _parentElement() {
-        if (this.parentNode) {
-            return this.parentNode.host;
-        }
+  get _parentElement() {
+    if (this.parentNode) {
+      return this.parentNode.host;
     }
+  }
 
-    bindMap(map) {
-        this._map = map;
-    }
+  bindMap(map) {
+    this._map = map;
+  }
 
-    _getSelectTemplate() {
-        return this._template(`
+  _getSelectTemplate() {
+    return this._template(`
             <select is="vl-select" id="test" data-vl-select data-vl-select-deletable data-vl-select-search-empty-text="Geen adres gevonden"></select>
         `);
-    };
+  };
 
-    _addSearchEventListener() {
-        if (!this.__searchEventListenerRegistered) {
-            this.__searchEventListenerRegistered = true;
-            this._selectElement.addEventListener('search', (event) => {
-                if (event && event.detail && event.detail.value) {
-                    fetch(this.searchUrl + event.detail.value).then((response) => {
-                        return response.json();
-                    }).then((data) => {
-                        if (data && data.SuggestionResult) {
-                            const resultaten = data.SuggestionResult.map((resultaat) => {
-                                return {
-                                    value: resultaat,
-                                    label: resultaat
-                                }
-                            });
-                            this._selectElement.choices = resultaten;
-                        }
-                    });
+  _addSearchEventListener() {
+    if (!this.__searchEventListenerRegistered) {
+      this.__searchEventListenerRegistered = true;
+      this._selectElement.addEventListener('search', (event) => {
+        if (event && event.detail && event.detail.value) {
+          fetch(this.searchUrl + event.detail.value).then((response) => {
+            return response.json();
+          }).then((data) => {
+            if (data && data.SuggestionResult) {
+              const resultaten = data.SuggestionResult.map((resultaat) => {
+                return {
+                  value: resultaat,
+                  label: resultaat
                 }
-            });
+              });
+              this._selectElement.choices = resultaten;
+            }
+          });
         }
+      });
     }
+  }
 
-    _addChoiceEventListener() {
-        if (!this.__choiceEventListenerRegistered) {
-            this.__choiceEventListenerRegistered = true;
-            this.__choiceEventListener = this._selectElement.addEventListener('choice', (event) => {
-                if (event && event.detail && event.detail.choice) {
-                    fetch(this.locationUrl + event.detail.choice.value).then((response) => {
-                        return response.json();
-                    }).then((data) => {
-                        if (data && data.LocationResult) {
-                            if (this._map) {
-                                this._map.zoomTo(
-                                    [data.LocationResult[0].BoundingBox.LowerLeft.X_Lambert72,
-                                    data.LocationResult[0].BoundingBox.LowerLeft.Y_Lambert72,
-                                    data.LocationResult[0].BoundingBox.UpperRight.X_Lambert72,
-                                    data.LocationResult[0].BoundingBox.UpperRight.Y_Lambert72]);
-                            }
-                        }
-                    });
-                }
-            });
+  _addChoiceEventListener() {
+    if (!this.__choiceEventListenerRegistered) {
+      this.__choiceEventListenerRegistered = true;
+      this.__choiceEventListener = this._selectElement.addEventListener('choice', (event) => {
+        if (event && event.detail && event.detail.choice) {
+          fetch(this.locationUrl + event.detail.choice.value).then((response) => {
+            return response.json();
+          }).then((data) => {
+            if (data && data.LocationResult) {
+              if (this._map) {
+                this._map.zoomTo(
+                  [data.LocationResult[0].BoundingBox.LowerLeft.X_Lambert72,
+                  data.LocationResult[0].BoundingBox.LowerLeft.Y_Lambert72,
+                  data.LocationResult[0].BoundingBox.UpperRight.X_Lambert72,
+                  data.LocationResult[0].BoundingBox.UpperRight.Y_Lambert72]);
+              }
+            }
+          });
         }
+      });
     }
+  }
 
-    onSelect(callback) {
-        this._onSelect = callback;
-    }
+  onSelect(callback) {
+    this._onSelect = callback;
+  }
 
   _configure() {
     customElements.whenDefined('vl-map').then(() => {
