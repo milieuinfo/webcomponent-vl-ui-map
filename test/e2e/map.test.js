f113388a7e3f2aa300727e2cbfa801a30ef5e422
cambiph <incubusattax@gmail.com>
2020-01-14 14:36:24 +0100

    Nieuwe opzet E2E testen

---
+++ test/e2e/map.test.js
@@ -0,0 +1,11 @@
+
+const { assert, driver } = require('vl-ui-core').Test;
+const VlMapPage = require('./pages/vl-map.page');
+
+describe('vl-map', async () => {
+    const vlMapPage = new VlMapPage(driver);
+
+    before(() => {
+        return vlMapPage.load();
+    });
+});
