c8396c5d7e16511237925dc31a89287fccd5f8f0
Pieter Beckers <pieter.beckers@vlaanderen.be>
2020-03-20 16:40:03 +0100

    UIG-372: review comments verwerken: omkeren circle stijl logica: verwijderen in select action

--- test/e2e/components/vl-map-select-action.js
+++ test/e2e/components/vl-map-select-action.js
@@ -1,23 +1,9 @@
 const VlMapAction = require('./vl-map-action');
-const VlMapLayerCircleStyle = require('./vl-map-layer-circle-style');
 
 class VlMapSelectAction extends VlMapAction {
 
     async isClustered() {
         return this.hasAttribute('cluster');
-    }
-
-    async _getCircleStyleElement() {
-        return this.findElement(By.css('vl-map-layer-circle-style'));
-    }
-
-    async hasCircleStyle() {
-        return this._getCircleStyleElement().then(() => true).catch(() => false);
-    }
-
-    async getCircleStyle() {
-        const style = await this._getCircleStyleElement();
-        return new VlMapLayerCircleStyle(this.driver, style);
     }
 }
 
