30bc68e7ce2a409b8e0b77b113b2df1198824721
Pieter Beckers <pieter.beckers@vlaanderen.be>
2020-03-20 15:07:55 +0100

    UIG-372: review comments verwerken: lachend kakske

--- test/e2e/components/vl-map.js
+++ test/e2e/components/vl-map.js
@@ -5,40 +5,40 @@ const VlMapSearch = require('./vl-map-search');
 const VlMapOverviewMap = require('./vl-map-overview-map');
 
 class VlMap extends VlElement {
-	async _getMap() {
-		return this.shadowRoot;
-	}
+    async _getMap() {
+        return this.shadowRoot;
+    }
 
-	async getBaseLayers() {
-		const childElements = await this.findElements(By.css(':scope > *'));
-		const tagNames = await Promise.all(childElements.map(element => element.getTagName()));
-		const baseLayerElements = childElements.filter((element, index) => tagNames[index].startsWith('vl-map-baselayer'));
-		return Promise.all(baseLayerElements.map(element => new VlMapBaseLayer(this.driver, element)));
-	}
-	
-	async isEscapeKeyDisabled() {
-		return this.hasAttribute("disable-escape-key");
-	}
+    async getBaseLayers() {
+        const childElements = await this.findElements(By.css(':scope > *'));
+        const tagNames = await Promise.all(childElements.map(element => element.getTagName()));
+        const baseLayerElements = childElements.filter((element, index) => tagNames[index].startsWith('vl-map-baselayer'));
+        return Promise.all(baseLayerElements.map(element => new VlMapBaseLayer(this.driver, element)));
+    }
 
-	async isRotationDisabled() {
-		return this.hasAttribute("disable-rotation");
-	}
-	
-	async isMouseWheelZoomDisabled() {
-		return this.hasAttribute("disable-mouse-wheel-zoom");
-	}
+    async isEscapeKeyDisabled() {
+        return this.hasAttribute("disable-escape-key");
+    }
 
-	async getOverviewMap() {
-		const map = await this._getMap();
-		await this.driver.wait(async () => {
+    async isRotationDisabled() {
+        return this.hasAttribute("disable-rotation");
+    }
+
+    async isMouseWheelZoomDisabled() {
+        return this.hasAttribute("disable-mouse-wheel-zoom");
+    }
+
+    async getOverviewMap() {
+        const map = await this._getMap();
+        await this.driver.wait(async () => {
             const overviewMaps = await map.findElements(By.css('.ol-overviewmap'));
-			return overviewMaps.length > 0;
-		});
-		const overviewMap = await map.findElement(By.css('.ol-overviewmap'));
-		return new VlMapOverviewMap(this.driver, overviewMap);
-	}
+            return overviewMaps.length > 0;
+        });
+        const overviewMap = await map.findElement(By.css('.ol-overviewmap'));
+        return new VlMapOverviewMap(this.driver, overviewMap);
+    }
 
-	async getActiveBaseLayerTitle() {
+    async getActiveBaseLayerTitle() {
         return this.driver.executeScript(`
 			return arguments[0].map.baseLayers.find((layer) => {
 				return layer.getVisible();
@@ -47,8 +47,8 @@ class VlMap extends VlElement {
     }
 
     async _getSearchElement() {
-		return this.driver.executeScript(`return arguments[0].shadowRoot.querySelector('vl-map-search')`, this);
-	}
+        return this.driver.executeScript(`return arguments[0].shadowRoot.querySelector('vl-map-search')`, this);
+    }
 
     async hasSearch() {
         const search = await this._getSearchElement();
