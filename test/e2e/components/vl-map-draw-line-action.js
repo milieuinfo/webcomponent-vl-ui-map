d31f856464c48dc1616c46a4a424d702c7c3b08f
Tom Coemans <tom.coemans@gmail.com>
2020-11-20 14:29:30 +0100

    double click

--- test/e2e/components/vl-map-draw-line-action.js
+++ test/e2e/components/vl-map-draw-line-action.js
@@ -5,7 +5,7 @@ class VlMapDrawLineAction extends VlMapAction {
     await this.scrollIntoView();
     const actions = this.driver.actions();
     await actions.move({origin: this, x: c1.x, y: c1.y}).click().perform();
-    return actions.move({origin: this, x: c2.x, y: c2.y}).click().click().perform();
+    return actions.move({origin: this, x: c2.x, y: c2.y}).doubleClick().perform();
   }
 }
 
