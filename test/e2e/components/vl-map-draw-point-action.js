fbdc6e9f48a610d2a3725da4cf78d38be53f5d84
Tom Coemans <tom.coemans@gmail.com>
2020-11-25 11:18:21 +0100

    fixed e2e test

--- test/e2e/components/vl-map-draw-point-action.js
+++ test/e2e/components/vl-map-draw-point-action.js
@@ -2,6 +2,7 @@ const VlMapAction = require('./vl-map-action');
 
 class VlMapDrawPointAction extends VlMapAction {
   async draw({x = 10, y = 10} = {x: 10, y: 10}) {
+    await this.scrollIntoView();
     const actions = this.driver.actions();
     return actions.move({origin: this, x: x, y: y}).click().perform();
   }
