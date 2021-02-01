1a5cef6fc99bc980e217b45bc6a62ba71378d833
Pieter Beckers <pieter.beckers@vlaanderen.be>
2021-02-01 11:53:40 +0100

    UIG-958: e2e test

---
+++ test/e2e/pages/vl-map-layer-style.page.js
@@ -0,0 +1,26 @@
+const VlMapLayerStyle = require('../components/vl-map-layer-style');
+const {Page, Config} = require('vl-ui-core').Test;
+
+class VlMapLayerStylePage extends Page {
+  async getStandardStyle() {
+    return this._getStyle('#map-standard-style');
+  }
+
+  async getLabelStyle() {
+    return this._getStyle('#map-label-style');
+  }
+
+  async getAdjustedStyle() {
+    return this._getStyle('#map-adjusted-style');
+  }
+
+  async load() {
+    await super.load(Config.baseUrl + '/demo/vl-map-layer-style.html');
+  }
+
+  async _getStyle(selector) {
+    return new VlMapLayerStyle(this.driver, selector);
+  }
+}
+
+module.exports = VlMapLayerStylePage;
