a41194b48fac7da6930c2f21836b571cd000c910
stefanborghys <stefan.borghys@seriousbusiness.be>
2021-02-01 13:55:31 +0100

    UIG-954: - allowed multiple spaces between coordinates - used named groups in the regex - used deep equals in the unit tests

--- src/lambert-coordinaat.js
+++ src/lambert-coordinaat.js
@@ -3,10 +3,10 @@
  * Lambert-coördinaat reguliere expressie. Staat toe:
  * - optioneel ongelimiteerd aantal spaties voor de start van het coördinaat
  * - optioneel linker haakje '('
- * - verplichte x-coördinaat, welke een decimaal (met een precisie van 2 getallen na de komma) of geheel getal kan zijn met 1-6 getallen
+ * - verplichte x-coördinaat, welke een decimaal of geheel getal kan zijn met 1-6 getallen voor de komma en een ongelimiteerd aantal na
  * - verplicht scheidingsteken, welke een komma ',' of puntkomma ';' kan zijn
- * - optionele spatie
- * - verplichte y-coördinaat, welke een decimaal (met een precisie van 2 getallen na de komma) of geheel getal kan zijn met 1-6 getallen
+ * - optionele ongelimiteerd aantal spaties
+ * - verplichte y-coördinaat, welke een decimaal of geheel getal kan zijn met 1-6 getallen voor de komma en een ongelimiteerd aantal na
  * - optioneel rechter haakje ')'
  *
  * E.g.:
@@ -16,7 +16,7 @@
  *
  * @type {RegExp}
  */
-const REGEX = /^\s*\(?(\d{1,6}\.\d{1,2}|\d{1,6})[,;]\u0020?(\d{1,6}\.\d{1,2}|\d{1,6})\)?/;
+const REGEX = /^\s*\(?(?<x>\d{1,6}\.\d{1,2}|\d{1,6})\d*[,;]\u0020*(?<y>\d{1,6}\.\d{1,2}|\d{1,6})\d*\)?/;
 
 class LambertCoordinaat {
   constructor(x, y) {
@@ -64,8 +64,8 @@ class LambertCoordinaat {
     }
     const resultaat = value.match(REGEX);
     if (resultaat) {
-      const x = Number(resultaat[1]);
-      const y = Number(resultaat[2]);
+      const x = Number(resultaat.groups.x);
+      const y = Number(resultaat.groups.y);
       return new LambertCoordinaat(x, y);
     }
     return undefined;
