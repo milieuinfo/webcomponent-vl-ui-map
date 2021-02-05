efa88bd003dcf256371d25906352613a6a5a4aee
coemans <tom.coemans@vlaanderen.be>
2021-02-05 14:17:58 +0100

    update dependencies (#213)

--- dist/vl-map-all.src.js
+++ dist/vl-map-all.src.js
@@ -1,23 +1,23 @@
 import {define, awaitScript} from 'vl-ui-core';
 
-import {VlMap} from 'vl-ui-map/dist/vl-map.src.js';
-import {VlMapOverviewMap} from 'vl-ui-map/dist/vl-map-overview-map.src.js';
-import {VlMapLayer} from 'vl-ui-map/dist/vl-map-layer.src.js';
-import {VlMapBaseLayer} from 'vl-ui-map/dist/vl-map-baselayer.src.js';
-import {VlMapBaseLayerGRBGray} from 'vl-ui-map/dist/vl-map-baselayer-grb-gray.src.js';
-import {VlMapBaseLayerGRB} from 'vl-ui-map/dist/vl-map-baselayer-grb.src.js';
-import {VlMapBaseLayerGRBOrtho} from 'vl-ui-map/dist/vl-map-baselayer-grb-ortho.src.js';
-import {VlMapAction} from 'vl-ui-map/dist/vl-map-action.src.js';
-import {VlMapLayerAction} from 'vl-ui-map/dist/vl-map-layer-action.src.js';
-import {VlMapSelectAction} from 'vl-ui-map/dist/vl-map-select-action.src.js';
-import {VlMapDrawPointAction} from 'vl-ui-map/dist/vl-map-draw-point-action.src.js';
-import {VlMapDrawLineAction} from 'vl-ui-map/dist/vl-map-draw-line-action.src.js';
-import {VlMapDrawPolygonAction} from 'vl-ui-map/dist/vl-map-draw-polygon-action.src.js';
-import {VlMapLayerStyle} from 'vl-ui-map/dist/vl-map-layer-style.src.js';
-import {VlMapLayerCircleStyle} from 'vl-ui-map/dist/vl-map-layer-circle-style.src.js';
-import {VlMapSearch} from 'vl-ui-map/dist/vl-map-search.src.js';
-import {VlMapSideSheet} from 'vl-ui-map/dist/vl-map-side-sheet.src.js';
-import {VlMapLayerSwitcher} from 'vl-ui-map/dist/vl-map-layer-switcher.src.js';
+import {VlMap} from '../dist/vl-map.src.js';
+import {VlMapOverviewMap} from '../dist/vl-map-overview-map.src.js';
+import {VlMapLayer} from '../dist/vl-map-layer.src.js';
+import {VlMapBaseLayer} from '../dist/vl-map-baselayer.src.js';
+import {VlMapBaseLayerGRBGray} from '../dist/vl-map-baselayer-grb-gray.src.js';
+import {VlMapBaseLayerGRB} from '../dist/vl-map-baselayer-grb.src.js';
+import {VlMapBaseLayerGRBOrtho} from '../dist/vl-map-baselayer-grb-ortho.src.js';
+import {VlMapAction} from '../dist/vl-map-action.src.js';
+import {VlMapLayerAction} from '../dist/vl-map-layer-action.src.js';
+import {VlMapSelectAction} from '../dist/vl-map-select-action.src.js';
+import {VlMapDrawPointAction} from '../dist/vl-map-draw-point-action.src.js';
+import {VlMapDrawLineAction} from '../dist/vl-map-draw-line-action.src.js';
+import {VlMapDrawPolygonAction} from '../dist/vl-map-draw-polygon-action.src.js';
+import {VlMapLayerStyle} from '../dist/vl-map-layer-style.src.js';
+import {VlMapLayerCircleStyle} from '../dist/vl-map-layer-circle-style.src.js';
+import {VlMapSearch} from '../dist/vl-map-search.src.js';
+import {VlMapSideSheet} from '../dist/vl-map-side-sheet.src.js';
+import {VlMapLayerSwitcher} from '../dist/vl-map-layer-switcher.src.js';
 
 Promise.all([
   awaitScript('vl-map-proj4', 'proj4/dist/proj4.js'),
