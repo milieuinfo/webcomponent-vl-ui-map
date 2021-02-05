2ef68c00fb5171e7b9d140086efff8dfd7556af7
Pieter Beckers <28813026+Pieter-Beckers@users.noreply.github.com>
2021-02-05 14:56:26 +0100

    Introductie feature label stijl (#206)

--- src/vl-map-layer-circle-style.js
+++ src/vl-map-layer-circle-style.js
@@ -1,5 +1,10 @@
 import {VlMapLayerStyle} from '/src/vl-map-layer-style.js';
-import {OlStyle, OlStyleCircle, OlStyleStroke, OlStyleText, OlStyleFill} from '/node_modules/vl-mapactions/dist/vl-mapactions.js';
+import {
+  OlStyle,
+  OlStyleCircle,
+  OlStyleStroke,
+  OlStyleFill,
+} from '/node_modules/vl-mapactions/dist/vl-mapactions.js';
 
 /**
  * VlMapLayerCircleStyle
@@ -74,7 +79,6 @@ export class VlMapLayerCircleStyle extends VlMapLayerStyle {
       const features = feature && feature.get ? (feature.get('features') || []) : [];
       const size = features.length || 1;
       const clusterMultiplier = size == 1 ? 1 : Math.max(1.5, size.toString().length);
-      const text = size > 1 ? size.toString() : '';
       let textColor = this.textColor;
       let color = this.color;
       let borderColor = this.borderColor;
@@ -111,15 +115,16 @@ export class VlMapLayerCircleStyle extends VlMapLayerStyle {
           }),
           radius: radius,
         }),
-        text: new OlStyleText({
-          text: text,
-          fill: new OlStyleFill({
-            color: textColor,
-          }),
-          offsetX: this.textOffsetX,
-          offsetY: this.textOffsetY,
-        }),
+        text: this._getTextStyle(feature, textColor),
       });
+    };
+  }
+
+  get featureLabelFunction() {
+    return (feature) => {
+      const features = feature && feature.get ? (feature.get('features') || []) : [];
+      const size = features.length || 1;
+      return size > 1 ? size.toString() : '';
     };
   }
 }
