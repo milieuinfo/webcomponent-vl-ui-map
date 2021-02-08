93cfe4a61c50b155a8b419e3c3c2c76ff82f9c32
stefanborghys <stefan.borghys@seriousbusiness.be>
2021-02-08 16:47:54 +0100

    UIG-1031: - javascript documentation typo fix

--- src/vl-map-select-action.js
+++ src/vl-map-select-action.js
@@ -27,7 +27,7 @@ export class VlMapSelectAction extends VlMapLayerAction {
   /**
    * Zet de stijl die een geselecteerd feature zal krijgen.
    *
-   * @param {VlMapLayerStyle} de stijl
+   * @param {VlMapLayerStyle} style
    */
   set style(style) {
     this._style = style;
