32f99c8cfc86f4ced6c11c5bb5abcad6cb5b84da
stefanborghys <stefan.borghys@seriousbusiness.be>
2021-02-08 11:00:10 +0100

    UIG-1006 - modify tryout

---
+++ src/vl-map-modify-point-action.js
@@ -0,0 +1,22 @@
+import {VlMapLayerAction} from '/src/vl-map-layer-action.js';
+import {VlModifyAction} from '/node_modules/vl-mapactions/dist/vl-mapactions.js';
+
+// TODO stefanborghys: 8/02/21  
+
+/**
+ * VlMapModifyPointAction
+ * @class
+ * @classdesc De kaart aanpas actie component.
+ *
+ * @extends VlMapLayerAction
+ *
+ * @see {@link https://www.github.com/milieuinfo/webcomponent-vl-ui-map/releases/latest|Release notes}
+ * @see {@link https://www.github.com/milieuinfo/webcomponent-vl-ui-map/issues|Issues}
+ * @see {@link https://webcomponenten.omgeving.vlaanderen.be/demo/vl-map-draw-actions.html|Demo}
+ */
+export class VlMapDrawPointAction extends VlMapLayerAction {
+  _createAction(layer) {
+    const options = {};
+    return new VlModifyAction(layer, this._callback, options);
+  }
+}
