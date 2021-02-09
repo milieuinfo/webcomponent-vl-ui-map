01d5dc6c8be6525de4ae949895c81452ab9cae61
Ruben Thielemans <rubenthielemans@hotmail.com>
2021-02-09 12:05:20 +0100

    Review comments

--- src/vl-map-side-sheet-menu-item.js
+++ src/vl-map-side-sheet-menu-item.js
@@ -1,5 +1,20 @@
 import {vlElement} from '/node_modules/vl-ui-core/dist/vl-core.js';
 
+
+/**
+ * VlMapSideSheetMenuItem
+ * @class
+ * @classdesc De menu item die verbonden is aan een side sheet.
+ *
+ * @extends VlElement
+ *
+ * @property {string} data-vl-title - Attribuut wordt gebruikt als titel van een menu item.
+ * @property {string} data-vl-href - Attribuut wordt gebruikt om via het href attribuut de link te koppelen aan een menu item.
+ *
+ * @see {@link https://www.github.com/milieuinfo/webcomponent-vl-ui-map/releases/latest|Release notes}
+ * @see {@link https://www.github.com/milieuinfo/webcomponent-vl-ui-map/issues|Issues}
+ * @see {@link https://webcomponenten.omgeving.vlaanderen.be/demo/vl-map-side-sheet.html|Demo}
+ */
 export class VlMapSideSheetMenuItem extends vlElement(HTMLElement) {
   static get _observedAttributes() {
     return ['title', 'href'];
@@ -9,15 +24,15 @@ export class VlMapSideSheetMenuItem extends vlElement(HTMLElement) {
     super(`
       <style>
         @import '/node_modules/vl-ui-link/dist/style.css';
-        .vl-map-sidesheet-menu-item {
+        .vl-map-side-sheet-menu-item {
           background: #e8ebee;
           padding: 2rem;
           margin: -2rem;
           margin-bottom: 3rem;
         }
       </style>
-      <div class="vl-map-sidesheet-menu-item">
-          <a id="vl-map-sidesheet-menu-item-link" is="vl-link" href="#">
+      <div class="vl-map-side-sheet-menu-item">
+          <a id="vl-map-side-sheet-menu-item-link" is="vl-link" href="#">
             <span is="vl-icon" data-vl-icon="arrow-left-fat"
                   data-vl-before></span><span id="title">Terug</span>
           </a>
@@ -29,14 +44,14 @@ export class VlMapSideSheetMenuItem extends vlElement(HTMLElement) {
     return this._shadow.querySelector('#title');
   }
 
+  get _hrefElement() {
+    return this._shadow.querySelector('#vl-map-side-sheet-menu-item-link');
+  }
+
   _titleChangedCallback(oldValue, newValue) {
     if (newValue) {
       this._titleElement.innerText = newValue;
     }
-  }
-
-  get _hrefElement() {
-    return this._shadow.querySelector('#vl-map-sidesheet-menu-item-link');
   }
 
   _hrefChangedCallback(oldValue, newValue) {
