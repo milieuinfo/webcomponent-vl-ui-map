f7fed27af89a75117b7ba43c1ebcd49a8a80a43c
stefanborghys <stefan.borghys@seriousbusiness.be>
2021-02-12 22:53:29 +0100

    UIG-1006: - linter

--- test/e2e/components/vl-map-modify-action.js
+++ test/e2e/components/vl-map-modify-action.js
@@ -1,13 +1,11 @@
 const VlMapAction = require('./vl-map-action');
 
 class VlMapModifyAction extends VlMapAction {
-  
   async movePointByCoordinates(from, to ) {
     const map = await this.getMap();
     await map.scrollIntoView();
-    await map.movePointByCoordinates(from , to );
+    await map.movePointByCoordinates(from, to );
   }
-  
 }
 
 module.exports = VlMapModifyAction;
