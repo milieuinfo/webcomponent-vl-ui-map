2a3d01cfeabeaf1ff3bb7c443ed0c687126f93df
Pieter Beckers <pieter.beckers@vlaanderen.be>
2021-02-12 15:01:06 +0100

    UIG-956: wms: andere tiled laag

--- test/e2e/map-wms-layer.test.js
+++ test/e2e/map-wms-layer.test.js
@@ -13,11 +13,11 @@ describe('vl-map-wms-layer', async () => {
     const layer = (await (await page.getMapWithTiledWmsLayer()).getTiledWmsLayers())[0];
 
     await assert.eventually.isTrue(layer.isVisible());
-    await assert.eventually.equal(layer.getName(), 'Beschermingszones');
-    await assert.eventually.equal(layer.getUrl(), 'https://www.dov.vlaanderen.be/geoserver/wms');
-    await assert.eventually.equal(layer.getLayers(), 'grondwater:beschermingszones_2014');
+    await assert.eventually.equal(layer.getName(), 'Gemeentegrenzen');
+    await assert.eventually.equal(layer.getUrl(), 'https://geoservices.informatievlaanderen.be/raadpleegdiensten/GRB/wms');
+    await assert.eventually.equal(layer.getLayers(), 'GEM_GRENS');
     await assert.eventually.equal(layer.getVersion(), '1.3.0');
-    await assert.eventually.equal(layer.getOpacity(), '0.7');
+    await assert.eventually.equal(layer.getOpacity(), '1');
   });
 
   it('als gebruiker kan ik de details van de image wms opvragen', async () => {
