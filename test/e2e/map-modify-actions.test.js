1a75655d7510c5e1daee1d526fadb58e9440d952
stefanborghys <stefan.borghys@seriousbusiness.be>
2021-02-15 14:29:11 +0100

    UIG-1006: - improved e2e tests as requested by the PR suggestion

--- test/e2e/map-modify-actions.test.js
+++ test/e2e/map-modify-actions.test.js
@@ -15,14 +15,7 @@ describe('vl-map-modify-action', async () => {
 
     const layer = await action.getLayer();
     await getDriver().wait(async () => (await layer.getNumberOfFeatures()) === 1);
-
-    const polygonFeature = await layer.getFeature(1);
-    const polygonCoordinates = polygonFeature.geometry.coordinates;
-    assert.lengthOf(polygonCoordinates, 1);
-
-    const points = polygonCoordinates[0];
-    assert.lengthOf(points, 4);
-
+    const points = (await layer.getFeature(1)).geometry.coordinates[0];
     const gent = points[0];
     const mechelen = points[1];
     const antwerpen = points[2];
@@ -33,28 +26,10 @@ describe('vl-map-modify-action', async () => {
     await getDriver().wait(async () => (await layer.getNumberOfFeatures()) === 1);
     assert.lengthOf(await layer.getFeatures(), 1);
 
-    const modifiedPolygonFeature = await layer.getFeature(1);
-    const modifiedPolygonCoordinates = modifiedPolygonFeature.geometry.coordinates;
-    assert.lengthOf(modifiedPolygonCoordinates, 1);
-
-    const modifiedPoints = modifiedPolygonCoordinates[0];
-    assert.lengthOf(modifiedPoints, 4);
-
-    const modifiedPoint = modifiedPoints[0];
-    assert.lengthOf(modifiedPoint, 2);
-    assert.notSameOrderedMembers(modifiedPoint, gent);
-
-    const unmodifiedPoint2 = modifiedPoints[1];
-    assert.lengthOf(unmodifiedPoint2, 2);
-    assert.sameOrderedMembers(unmodifiedPoint2, mechelen);
-
-    const unmodifiedPoint3 = modifiedPoints[2];
-    assert.lengthOf(unmodifiedPoint3, 2);
-    assert.sameOrderedMembers(unmodifiedPoint3, antwerpen);
-
-    const unmodifiedPoint4 = modifiedPoints[3];
-    assert.lengthOf(unmodifiedPoint4, 2);
-    assert.sameOrderedMembers(unmodifiedPoint4, leuven);
+    const modifiedGeometry = (await layer.getFeature(1)).geometry;
+    assert.include(modifiedGeometry, {type: 'Polygon'});
+    assert.includeDeepMembers(modifiedGeometry.coordinates[0], [mechelen, antwerpen, leuven]);
+    assert.notIncludeDeepMembers(modifiedGeometry.coordinates[0], [gent]);
   });
 
   it('als gebruiker kan ik een punt aanpassen op een kaart', async () => {
@@ -62,7 +37,6 @@ describe('vl-map-modify-action', async () => {
 
     const layer = await action.getLayer();
     await getDriver().wait(async () => (await layer.getNumberOfFeatures()) === 2);
-
     const mechelen = (await layer.getFeature(1)).geometry.coordinates;
     const antwerpen = (await layer.getFeature(2)).geometry.coordinates;
 
@@ -71,15 +45,11 @@ describe('vl-map-modify-action', async () => {
     await getDriver().wait(async () => (await layer.getNumberOfFeatures()) === 2);
     assert.lengthOf(await layer.getFeatures(), 2);
 
-    const modifiedPoint1Feature = await layer.getFeature(1);
-    const modifiedCoordinates = modifiedPoint1Feature.geometry.coordinates;
-    assert.lengthOf(modifiedCoordinates, 2);
-    assert.notSameOrderedMembers(modifiedCoordinates, mechelen);
+    const modifiedGeometry = (await layer.getFeature(1)).geometry;
+    assert.include(modifiedGeometry, {type: 'Point'});
+    assert.notIncludeDeepMembers(modifiedGeometry.coordinates, mechelen);
 
-    const unmodifiedPoint2Feature = await layer.getFeature(2);
-    const unmodifiedCoordinates = unmodifiedPoint2Feature.geometry.coordinates;
-    assert.lengthOf(unmodifiedCoordinates, 2);
-    assert.sameOrderedMembers(unmodifiedCoordinates, antwerpen);
+    assert.deepOwnInclude((await layer.getFeature(2)).geometry, {type: 'Point', coordinates: antwerpen});
   });
 
   it('als gebruiker kan ik een lijn aanpassen op een kaart', async () => {
@@ -87,11 +57,7 @@ describe('vl-map-modify-action', async () => {
 
     const layer = await action.getLayer();
     await getDriver().wait(async () => (await layer.getNumberOfFeatures()) === 1);
-
-    const lineFeature = await layer.getFeature(1);
-    const lineCoordinates = lineFeature.geometry.coordinates;
-    assert.lengthOf(lineCoordinates, 2);
-
+    const lineCoordinates = (await layer.getFeature(1)).geometry.coordinates;
     const mechelen = lineCoordinates[0];
     const antwerpen = lineCoordinates[1];
 
@@ -100,17 +66,10 @@ describe('vl-map-modify-action', async () => {
     await getDriver().wait(async () => (await layer.getNumberOfFeatures()) === 1);
     assert.lengthOf(await layer.getFeatures(), 1);
 
-    const modifiedLineFeature = await layer.getFeature(1);
-    const modifiedLineCoordinates = modifiedLineFeature.geometry.coordinates;
-    assert.lengthOf(modifiedLineCoordinates, 2);
-
-    const unmodifiedPoint = modifiedLineCoordinates[0];
-    assert.lengthOf(unmodifiedPoint, 2);
-    assert.sameOrderedMembers(unmodifiedPoint, mechelen);
-
-    const modifiedPoint = modifiedLineCoordinates[1];
-    assert.lengthOf(modifiedPoint, 2);
-    assert.notSameOrderedMembers(modifiedPoint, antwerpen);
+    const modifiedGeometry = (await layer.getFeature(1)).geometry;
+    assert.include(modifiedGeometry, {type: 'LineString'});
+    assert.includeDeepMembers(modifiedGeometry.coordinates[0], mechelen);
+    assert.notIncludeDeepMembers(modifiedGeometry.coordinates[0], antwerpen);
   });
 
   it('als gebruiker zie ik dat de aanpas actie actief staat', async () => {
