98cec634a1aa279f669aab61731f19def032fcf0
wautergu <guy.wauters@vlaanderen.be>
2021-02-17 14:22:10 +0100

    wait wegwerken

--- test/e2e/map-delete-action.test.js
+++ test/e2e/map-delete-action.test.js
@@ -19,38 +19,20 @@ describe('vl-map-delete-action', async () => {
   it('kan features deleten door er op te klikken', async () => {
     const deleteAction = await vlMapPage.getDeleteAction();
     const layer = await deleteAction.getLayer();
-    await driver.wait(async () => {
-      const aantalFeatures = await layer.getNumberOfFeatures();
-      return aantalFeatures == 3;
-    });
+    await assert.eventually.equal(layer.getNumberOfFeatures(), 3);
     await deleteAction.removeFeature(1);
-    await driver.wait(async () => {
-      const aantalFeatures = await layer.getNumberOfFeatures();
-      return aantalFeatures == 2;
-    });
+    await assert.eventually.equal(layer.getNumberOfFeatures(), 2);
     await deleteAction.removeFeature(2);
-    await driver.wait(async () => {
-      const aantalFeatures = await layer.getNumberOfFeatures();
-      return aantalFeatures == 1;
-    });
+    await assert.eventually.equal(layer.getNumberOfFeatures(), 1);
     await deleteAction.removeFeature(3);
-    await driver.wait(async () => {
-      const aantalFeatures = await layer.getNumberOfFeatures();
-      return aantalFeatures == 0;
-    });
+    await assert.eventually.equal(layer.getNumberOfFeatures(), 0);
   });
 
   it('kan features deleten door rechthoek te trekken over alle features die weg mogen', async () => {
     const deleteAction = await vlMapPage.getDeleteAction();
     const layer = await deleteAction.getLayer();
-    await driver.wait(async () => {
-      const aantalFeatures = await layer.getNumberOfFeatures();
-      return aantalFeatures == 3;
-    });
+    await assert.eventually.equal(layer.getNumberOfFeatures(), 3);
     await deleteAction.removeAllInRectangle([80000, 210000], [230000, 170000]);
-    await driver.wait(async () => {
-      const aantalFeatures = await layer.getNumberOfFeatures();
-      return aantalFeatures == 0;
-    });
+    await assert.eventually.equal(layer.getNumberOfFeatures(), 0);
   });
 });
