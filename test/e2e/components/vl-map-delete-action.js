1fbf2880805e06eba0511c074b2ecf4fa9008b9f
pascalriquier <pascal.riquier@vlaanderen.be>
2021-02-18 13:54:19 +0100

    Introductie interactie om punt, lijn en polygoon te kunnen verwijderen (#219)

---
+++ test/e2e/components/vl-map-delete-action.js
@@ -0,0 +1,29 @@
+const VlMapAction = require('./vl-map-action');
+const VlMapLayers = require('./vl-map-layers');
+
+class VlMapDeleteAction extends VlMapAction {
+  async getLayer() {
+    return await VlMapLayers.asLayer(this.driver, await this.parent());
+  }
+
+  async removeFeature(id) {
+    const layer = await this.getLayer();
+    const map = await this.getMap();
+    await map.scrollIntoView();
+    const coordinateForFeature = await layer.getCoordinateForFeature(id);
+    const numberOfFeaturesBefore = await layer.getNumberOfFeatures();
+    await map.clickOnCoordinates(coordinateForFeature);
+    await this.driver.wait(async () => {
+      const numberOfFeatures = await layer.getNumberOfFeatures();
+      return numberOfFeatures == numberOfFeaturesBefore - 1;
+    });
+  }
+
+  async removeAllInRectangle(coordinatesTopLeft, coordinatesBottomRight) {
+    const map = await this.getMap();
+    await map.scrollIntoView();
+    await map.dragRectangle(coordinatesTopLeft, coordinatesBottomRight);
+  }
+}
+
+module.exports = VlMapDeleteAction;
