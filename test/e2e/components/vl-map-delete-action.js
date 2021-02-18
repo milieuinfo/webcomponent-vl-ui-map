4e33da89643db391e3d96984ec08a7f93c68cbe2
Pascal Riquier <pascal.riquier@vlaanderen.be>
2021-02-18 13:01:05 +0100

    UIG-1008 wachten tot feature weg is encapsuleren in delete actie

--- test/e2e/components/vl-map-delete-action.js
+++ test/e2e/components/vl-map-delete-action.js
@@ -11,7 +11,12 @@ class VlMapDeleteAction extends VlMapAction {
     const map = await this.getMap();
     await map.scrollIntoView();
     const coordinateForFeature = await layer.getCoordinateForFeature(id);
+    const numberOfFeaturesBefore = await layer.getNumberOfFeatures();
     await map.clickOnCoordinates(coordinateForFeature);
+    await this.driver.wait(async () => {
+      const numberOfFeatures = await layer.getNumberOfFeatures();
+      return numberOfFeatures == numberOfFeaturesBefore - 1;
+    });
   }
 
   async removeAllInRectangle(coordinatesTopLeft, coordinatesBottomRight) {
